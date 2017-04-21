1d7a573c5139f40412af00bf2c76d5b802dacef2
Sphere <Sphere@Lori>
2017-04-21 23:32:59 +0800

    Fixing user functions, adding encrypotion

---
+++ src/java/Deencrypt.java
@@ -0,0 +1,56 @@
+
+
+import java.security.MessageDigest;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+
+public class Deencrypt {
+    public static String encrypt(String strClearText,String strKey) throws Exception{
+        String strData="";
+        try {
+            SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
+            Cipher cipher=Cipher.getInstance("Blowfish");
+            cipher.init(Cipher.ENCRYPT_MODE, skeyspec);
+            byte[] encrypted=cipher.doFinal(strClearText.getBytes());
+            strData=new String(encrypted);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception(e);
+        }
+        return strData;
+    }
+    public static String decrypt(String strEncrypted,String strKey) throws Exception{
+        String strData="";
+
+        try {
+            SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
+            Cipher cipher=Cipher.getInstance("Blowfish");
+            cipher.init(Cipher.DECRYPT_MODE, skeyspec);
+            byte[] decrypted=cipher.doFinal(strEncrypted.getBytes());
+            strData=new String(decrypted);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception(e);
+        }
+        return strData;
+    }
+    public static String sha256(String base) {
+    try{
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(base.getBytes("UTF-8"));
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+    catch(Exception ex){
+       throw new RuntimeException(ex);
+    }
+}
+}
